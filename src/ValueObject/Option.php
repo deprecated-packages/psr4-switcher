9079c98f9f2f7764c3feb8c58057724942d5acdb
Tomas Votruba <tomas.vot@gmail.com>
2020-09-18 17:13:58 +0200

    [EasyCI] Add custom keys (#177)
    * [EasyCI] Add custom keys
    
    * simplify generate command
    
    * decouple SonarConfigDataPrinter

---
+++ src/ValueObject/Option.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\Psr4Switcher\ValueObject;
+
+final class Option
+{
+    /**
+     * @var string
+     */
+    public const COMPOSER_JSON = 'composer-json';
+}
